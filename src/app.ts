cb529e7af070821aef2e54a8a23d27ba1d3458d7
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-10-07 14:03:35 +0100

    securing api

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express, { Express } from 'express';
 import * as dotenv from 'dotenv';
 import path from 'path';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
 
 import connectDB from './config/db';
 import UserRoute from './routes/userRoute';
 import URLRoute from './routes/urlRoute';
 import errorHandler from './middlewares/errorHandler';
 import notFound from './middlewares/notFound';
+import apiLimiter from './middlewares/rateLimit';
 
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 connectDB();
@@ -17,6 +20,9 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(apiLimiter);
+app.use(helmet());
+app.use(mongoSanitize());
 
 app.use('/', UserRoute);
 app.use('/urls', URLRoute);
